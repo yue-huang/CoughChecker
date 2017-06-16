38c6eba48b19c5bbd140c5ab958128750557d1e7
Yue Huang <yue.daphne.huang@gmail.com>
2017-06-15 22:20:47 -0700

    mild_severe_v1

--- flask/flaskexample/a_model.py
+++ flask/flaskexample/a_model.py
@@ -5,6 +5,10 @@ def modelIt(user_input, model_file_name = 'flaskexample/data/finalized_model.sav
     import numpy as np
     est_loaded = pickle.load(open(model_file_name, 'rb'))
     testprob = est_loaded.predict_proba(np.array([user_input])).item((0,1))
-    #print('the test prob is:', testprob)
-    return "{0:.0f}%".format(testprob*100)
+    y_predict = est_loaded.predict(np.array([user_input]))[0]
+    if y_predict == 1: label = ['sorry that you may have a severe condition. Please visit your healthcare provider soon!','severe_condition.png']
+    if y_predict == 0: label = ['congratulations! You have a mild condition. Feel free to recover by yourself. Please keep monitoring your symptoms.','mild_condition.png']
+    print('the test prob and predicted class are:', testprob, y_predict)
+    #return "{0:.0f}%".format(testprob*100)
+    return label
 #modelIt([1,1,1,1,1])
--- flask/flaskexample/views.py
+++ flask/flaskexample/views.py
@@ -1,7 +1,7 @@
 # some comment here
 from .a_model import modelIt
 from flaskexample import app
-from flask import render_template, request
+from flask import render_template, request, send_from_directory
 
 @app.route('/')
 @app.route('/index')
@@ -10,8 +10,12 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def result():
-    user_input = [ int(request.form.get(feature))
-                   for feature in  ['q1','q2','q3','q4','q5'] ]
+    user_input = []
+    for feature in ['q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11','q12','q13','q14','q15','q16','q17','q18','q19','q20']:
+        if request.form.get(feature): user_input.append(1)
+        else: user_input.append(0)
+    #user_input = [ int(request.form.get(feature))
+    #               for feature in  ['q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11','q12','q13','q14','q15','q16','q17','q18','q19','q20'] ]
     print(user_input)
-    prob = modelIt(user_input)
-    return render_template("result.html", user_input = user_input, prob = prob)
+    label = modelIt(user_input)
+    return render_template("result.html", user_input = user_input, label = label)
