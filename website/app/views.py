1a13f990a538f01fa1dde269a632d09f35fcc405
Yue Huang <yue.daphne.huang@gmail.com>
2017-06-22 00:23:23 -0700

    website v2.1

--- website/app/views.py
+++ website/app/views.py
@@ -11,11 +11,44 @@ def index():
 @app.route('/result', methods=['POST'])
 def result():
     user_input = []
-    for feature in ['q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11','q12','q13','q14','q15']:
-        if request.form.get(feature): user_input.append(1)
-        else: user_input.append(0)
+
+    if request.form.get('q1'):
+        user_input.append(int(request.form.get('q1')))
+    else:
+        user_input.append(92/2)
+
+    if request.form.get('q2'):
+        user_input.append(int(request.form.get('q2')))
+    else:
+        user_input.append((77-19)/2)
+
+    if request.form.get('q3'):
+        user_input.append(1)
+    else:
+        user_input.append(0)
+
+    if request.form.get('q4'):
+        symptom_type = int(request.form.get('q4'))
+        if symptom_type == 1:
+            user_input = user_input + [1,0,0]
+        elif symptom_type == 2:
+            user_input = user_input + [0,1,0]
+        elif symptom_type == 3:
+            user_input = user_input + [0,0,1]
+        else:
+            user_input = user_input + [0,0,0]
+    else:
+        user_input = user_input + [0,0,0]
+
+    for feature in ['q7','q8','q9','q10','q11','q12','q13','q14','q15']:
+        if request.form.get(feature):
+            user_input.append(1)
+        else:
+            user_input.append(0)
+
     #user_input = [ int(request.form.get(feature))
-    #               for feature in  ['q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11','q12','q13','q14','q15','q16','q17','q18','q19','q20'] ]
+    #               for feature in  ['q1','q2','q3']
+    #               ]
     print(user_input)
     label = modelIt(user_input)
     return render_template("result.html", user_input = user_input, label = label)
